48198d73c69599adea8cc1886c9c6b4b7491f5c6
Jackagency <diablo372@yandex.ru>
2022-12-10 15:19:48 +0200

    First commit

---
+++ ConsoleApplication1/Properties/Tests/TestNewOrder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.Tools;
+using FlaUI.UIA3;
+using NUnit.Framework;
+
+namespace ConsoleApplication1.Properties.Tests
+{
+  
+    public class TestNewOrder
+    {
+        [Test]
+        public void TestsCtrader()
+        {
+            var cTrader =
+                Application.Launch(
+                    @"C:\Users\USER\AppData\Local\Spotware\cTrader\abb70432efbee65d18af69e79fe8efe1\cTrader.exe");
+            
+
+            var _mainWindow = cTrader.GetMainWindow(new UIA3Automation());
+
+            var cf = new ConditionFactory(new UIA3PropertyLibrary());
+            
+            var tradeWatchPannel = WaitForElement(() => _mainWindow.FindFirstDescendant(cf.ByAutomationId("TradeWatchTabControl_AId")));
+
+            var exampleButton = WaitForElement(() => tradeWatchPannel.FindFirstDescendant(cf.ByAutomationId("CreateNewMarketOrderButton_AId")));
+            exampleButton.AsButton().Click();
+            _mainWindow.FindFirstDescendant(cf.ByAutomationId("SpinnerContent_AId")).AsTextBox().Enter("1400");
+            _mainWindow.FindFirstDescendant(cf.ByAutomationId("QuantityOrVolumeTextBox_AId")).AsTextBox().Enter("5");
+            _mainWindow.FindFirstDescendant(cf.ByAutomationId("CheckBoxStopLoss_AId")).AsCheckBox().Click();
+            _mainWindow.FindFirstDescendant(cf.ByAutomationId("CheckBoxTakeProfit_AId")).AsCheckBox().Click();
+            _mainWindow.FindFirstDescendant(cf.ByName("Place Order")).AsButton().Click();
+
+        }
+        private T WaitForElement<T>(Func<T> getter)
+        {
+            var retry = Retry.WhileNull<T>(
+                () => getter(),
+                TimeSpan.FromMilliseconds(20000));
+
+            if (!retry.Success)
+            {
+                Assert.Fail("Failed to get an element within a wait timeout");
+            }
+
+            return retry.Result;
+        }
+    }
+}
---
+++ ConsoleApplication1/Properties/Tests/TestTask.cs
@@ -0,0 +1,28 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.UIA3;
+using NUnit.Framework;
+
+
+namespace ConsoleApplication1.Properties.Tests
+{
+    public class TestTask
+    {
+        [Test]
+        public void TestsJustProgram()
+        {
+            var cTrader =
+                Application.Launch(
+                    @"C:\Users\USER\Desktop\JustProgram\TestTask.Wpf.exe");
+
+            var mainWindow = cTrader.GetMainWindow(new UIA3Automation());
+
+            var cf = new ConditionFactory(new UIA3PropertyLibrary());
+
+            mainWindow.FindFirstDescendant(cf.ByAutomationId("PART_EditableTextBox")).AsComboBox().Select(3);
+            //mainWindow.FindFirstDescendant(cf.ByAutomationId("PART_EditableTextBox")).AsComboBox().Select("5000");
+        }
+        
+    }
+}
